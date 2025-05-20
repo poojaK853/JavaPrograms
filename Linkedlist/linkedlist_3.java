0e38a60c6f127f4a9add7cea7d735f423dc5782f
poojaK853 <152719914+poojaK853@users.noreply.github.com>
2025-05-20 22:39:53 +0530

    linkedlist_3.java

--- Linkedlist/linkedlist_3.java
+++ Linkedlist/linkedlist_3.java
@@ -1,23 +1,28 @@
 /*Write a Java program to insert the specified element at the end of a linked list.( using
 l_listobj.offerLast("Pink"))*/
-
 package program1;
+
 import java.util.*;
+
 public class program1_c3 {
-	public static void main(String[] args) {
-			// TODO Auto-generated method stub
+    public static void main(String[] args) {
+        // Create a LinkedList of Strings
+        LinkedList<String> ls = new LinkedList<>();
 
-			LinkedList<String> ls = new LinkedList<>();
-			ls.add("Orange");
-			ls.add("geen");
-			ls.add("pink");
-			ls.add("red");
-			ls.add("Blue");
-			System.out.println(ls);
-			ls.addLast("pink");
-			System.out.println(ls);
-		
-		}
+        // Add elements to the LinkedList
+        ls.add("Orange");
+        ls.add("geen");  // Possibly a typo, intended "green"
+        ls.add("pink");
+        ls.add("red");
+        ls.add("Blue");
 
-	}
+        // Print the LinkedList to show the current elements
+        System.out.println(ls);
 
+        // Add another element at the end of the LinkedList using addLast()
+        ls.addLast("pink");
+
+        // Print the LinkedList again to show the updated list with the new element
+        System.out.println(ls);
+    }
+}
