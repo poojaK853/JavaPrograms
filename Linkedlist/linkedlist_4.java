5ad808f379f84906b4838ac1ada8335c3ad06a1b
poojaK853 <152719914+poojaK853@users.noreply.github.com>
2025-05-20 22:40:34 +0530

    linkedlist_4.java

--- Linkedlist/linkedlist_4.java
+++ Linkedlist/linkedlist_4.java
@@ -2,20 +2,25 @@
 l_listobj.get(p) )*/
 
 package program1;
+
 import java.util.*;
+
 public class program1_c4 {
-	public static void main(String[] args) {
-			// TODO Auto-generated method stub
-			LinkedList<String> ls = new LinkedList<>();
-			ls.add("Orange");
-			ls.add("geen");
-			ls.add("pink");
-			ls.add("red");
-			ls.add("Blue");
-			for(int i=0;i<ls.size();i++) {
-				System.out.println("index: "+i+" value:"+ls.get(i));
-			}
-		}
+    public static void main(String[] args) {
+        // Create a LinkedList of Strings
+        LinkedList<String> ls = new LinkedList<>();
 
-	}
+        // Add elements to the LinkedList
+        ls.add("Orange");
+        ls.add("geen");  // Possible typo, probably meant "green"
+        ls.add("pink");
+        ls.add("red");
+        ls.add("Blue");
 
+        // Iterate through the LinkedList using a for loop by index
+        for (int i = 0; i < ls.size(); i++) {
+            // Print the current index and the element at that index
+            System.out.println("index: " + i + " value: " + ls.get(i));
+        }
+    }
+}
