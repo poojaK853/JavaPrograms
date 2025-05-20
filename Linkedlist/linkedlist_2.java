ca17249be2086b15ed831dfcea21136ea71dce76
poojaK853 <152719914+poojaK853@users.noreply.github.com>
2025-05-20 22:39:12 +0530

    linkedlist_2.java

--- Linkedlist/linkedlist_2.java
+++ Linkedlist/linkedlist_2.java
@@ -2,28 +2,40 @@
 objlist.descendingIterator())*/
 
 package program1;
+
 import java.util.Iterator;
 import java.util.LinkedList;
-import java.util.List;
-import java.util.*;
+
 public class program1_c2 {
-	public static void main(String[] args) {
-			// TODO Auto-generated method stub
-			LinkedList<String>	ls = new LinkedList<>();
-			ls.add("Orange");
-			ls.add("geen");
-			ls.add("pink");
-			ls.add("red");
-			ls.add("Blue");
-//			ListIterator<String> listIterator = ls.listIterator(ls.size());
-//	        while (listIterator.hasPrevious()) {
-//	            System.out.println(listIterator.previous());
-//	        }
-			Iterator iterator =  ls.descendingIterator();
-			while (iterator.hasNext()) {
-				System.out.println(iterator.next());
-				}
-		}
+    public static void main(String[] args) {
+        // Create a LinkedList of Strings
+        LinkedList<String> ls = new LinkedList<>();
+
+        // Add elements to the LinkedList
+        ls.add("Orange");
+        ls.add("geen");   // Note: probably a typo, should be "green"
+        ls.add("pink");
+        ls.add("red");
+        ls.add("Blue");
+
+        /*
+        // Alternative way to iterate in reverse order using ListIterator
+        ListIterator<String> listIterator = ls.listIterator(ls.size());
+        while (listIterator.hasPrevious()) {
+            System.out.println(listIterator.previous());
+        }
+        */
+
+        // Create a descending iterator to traverse the list in reverse order
+        Iterator<String> iterator = ls.descendingIterator();
+
+        // Iterate over the LinkedList in reverse and print each element
+        while (iterator.hasNext()) {
+            System.out.println(iterator.next());
+        }
+    }
+}
+
 
 	}
 
