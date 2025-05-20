a12fbb6aab64040679ed3af0e29c2914e575404b
poojaK853 <152719914+poojaK853@users.noreply.github.com>
2025-05-20 22:35:01 +0530

    arraylist_4.java

--- Arraylist/arraylist_4.java
+++ Arraylist/arraylist_4.java
@@ -1,20 +1,28 @@
 /*Write a java program for getting different colors through ArrayList interface and extract the
 elements 1st and 2nd from the ArrayList object by using SubList()*/
 
-import java.util.*;
+import java.util.*;  // Importing the java.util package to use List, ArrayList, etc.
+
 public class p4 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		List<String> ls = new ArrayList<>();
-		ls.add("Orange");
-		ls.add("geen");
-		ls.add("pink");
-		ls.add("red");
-		ls.add("Blue");
-		System.out.println(ls);
-		System.out.println("using sublist:");
-		System.out.println(ls.subList(0,2));
-	}
+    public static void main(String[] args) {
+        // Creating a list of strings to store color names
+        List<String> ls = new ArrayList<>();
 
+        // Adding elements (colors) to the list
+        ls.add("Orange");
+        ls.add("geen");   // Possibly a typo, should be "green"
+        ls.add("pink");
+        ls.add("red");
+        ls.add("Blue");
+
+        // Printing the full list
+        System.out.println(ls);  // Output: [Orange, geen, pink, red, Blue]
+
+        // Using subList() to get a portion of the list
+        // subList(fromIndex, toIndex) returns elements from fromIndex (inclusive) to toIndex (exclusive)
+        System.out.println("using sublist:");
+        System.out.println(ls.subList(0, 2));  // Output: [Orange, geen]
+    }
 }
+
