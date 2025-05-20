c867543fa03eb68f36c4c7058c4552af226aaf46
poojaK853 <152719914+poojaK853@users.noreply.github.com>
2025-05-20 22:31:21 +0530

    arraylist_1.java

--- Arraylist/arraylist_1.java
+++ Arraylist/arraylist_1.java
@@ -3,22 +3,28 @@
 //( Hint : Use ArrayListObj.contains() ) 
 
 
-import java.util.*;
+import java.util.*;  // Importing utility classes like List and ArrayList
+
 public class p1 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		List<String> ls = new ArrayList<>();
-		ls.add("Orange");
-		ls.add("geen");
-		ls.add("pink");
-		ls.add("red");
-		System.out.println(ls);
-		if(ls.contains("red")) {
-			System.out.println("available");
-		}else {
-			System.out.println("available not");
-		}
-	}
+    public static void main(String[] args) {
+        // Creating a List of strings to store color names
+        List<String> ls = new ArrayList<>();
 
+        // Adding elements (color names) to the list
+        ls.add("Orange");
+        ls.add("geen");    // Likely a typo, should be "green"
+        ls.add("pink");
+        ls.add("red");
+
+        // Printing the entire list
+        System.out.println(ls);
+
+        // Checking if the list contains the string "red"
+        if(ls.contains("red")) {
+            System.out.println("available");  // If found, print "available"
+        } else {
+            System.out.println("available not");  // If not found, print "available not"
+        }
+    }
 }
