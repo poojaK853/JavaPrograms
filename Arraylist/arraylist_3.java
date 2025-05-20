d20147ba303a0252467c77916d359f143cf1d0a0
poojaK853 <152719914+poojaK853@users.noreply.github.com>
2025-05-20 22:33:56 +0530

    arraylist_3.java

--- Arraylist/arraylist_3.java
+++ Arraylist/arraylist_3.java
@@ -1,22 +1,25 @@
 /*Write a java program for getting different colors through ArrayList interface and sort them
 using Collections.sort( ArrayListObj)*/
 
-import java.util.*;
+import java.util.*;  // Importing utility classes including List, ArrayList, and Collections
+
 public class p3 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		List<String> ls = new ArrayList<>();
-		ls.add("Orange");
-		ls.add("geen");
-		ls.add("pink");
-		ls.add("red");
-		ls.add("Blue");
-		System.out.println("Before sorting:");
-		System.out.println(ls);
-		Collections.sort(ls);
-		System.out.println("After sorting:");
-		System.out.println(ls);
-	}
+    public static void main(String[] args) {
+        // Creating a List of Strings to store color names
+        List<String> ls = new ArrayList<>();
 
-}
+        // Adding elements (color names) to the list
+        ls.add("Orange");
+        ls.add("geen");    // Likely a typo, should be "green"
+        ls.add("pink");
+        ls.add("red");
+        ls.add("Blue");
+
+        // Printing the list before sorting
+        System.out.println("Before sorting:");
+        System.out.println(ls);  // Output will show the order of insertion
+
+        // Sorting the list in ascending (lexicographical) order
+        Collections.sort(ls);
+
