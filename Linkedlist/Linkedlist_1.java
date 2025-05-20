7c7085e0fe35fad8c2289e18438bf7d877e86813
poojaK853 <152719914+poojaK853@users.noreply.github.com>
2025-05-20 22:37:07 +0530

    Linkedlist_1.java

--- Linkedlist/Linkedlist_1.java
+++ Linkedlist/Linkedlist_1.java
@@ -1,22 +1,30 @@
 /*Write a Java program to iterate through all elements in a linked list starting at the
 specified position ( 2nd) using iterator ( hint : Iterator iteratorobj = listobj.listIterator(1))*/
 
-package program1;
-import java.util.*;
+package program1;  // Declaring the package (optional, based on your project structure)
+
+import java.util.*;  // Importing utility classes including List, LinkedList, Iterator
+
 public class program1_c1 {
-	public static void main(String[] args) {
-			// TODO Auto-generated method stub
-			List<String>	ls = new LinkedList<>();
-			ls.add("Orange");
-			ls.add("geen");
-			ls.add("pink");
-			ls.add("red");
-			ls.add("Blue");
-			Iterator iterator = ls.listIterator(2);
-			while (iterator.hasNext()) {
-			System.out.println(iterator.next());
-			}
-		}
+    public static void main(String[] args) {
+        // Creating a LinkedList of Strings
+        List<String> ls = new LinkedList<>();
 
-	}
+        // Adding color names to the list
+        ls.add("Orange");
+        ls.add("geen");    // Likely a typo, should be "green"
+        ls.add("pink");
+        ls.add("red");
+        ls.add("Blue");
 
+        // Creating a ListIterator starting from index 2
+        // So the iteration will start from "pink"
+        Iterator iterator = ls.listIterator(2);
+
+        // Iterating from index 2 to the end of the list
+        while (iterator.hasNext()) {
+            // Printing each element from index 2 onward
+            System.out.println(iterator.next());
+        }
+    }
+}
