6bffa2952fe0537f99f8bab65a3bc9d7bef4a58a
poojaK853 <152719914+poojaK853@users.noreply.github.com>
2025-05-20 22:32:47 +0530

    arraylist_2.java

--- Arraylist/arraylist_2.java
+++ Arraylist/arraylist_2.java
@@ -1,22 +1,35 @@
 /* Write a java program for getting different colors through ArrayList interface and remove the
 2nd element and color "Blue" from the ArrayList*/
 
-import java.util.*;
+import java.util.*;  // Importing the java.util package for List and ArrayList
+
 public class p2 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		List<String> ls = new ArrayList<>();
-		ls.add("Orange");
-		ls.add("geen");
-		ls.add("pink");
-		ls.add("red");
-		ls.add("Blue");
-		System.out.println(ls);
-		ls.remove(2);
-		System.out.println(ls);
-		ls.remove("Blue");
-		System.out.println(ls);
-	}
+    public static void main(String[] args) {
+        // Creating a List of Strings using ArrayList
+        List<String> ls = new ArrayList<>();
 
+        // Adding elements (colors) to the list
+        ls.add("Orange");
+        ls.add("geen");    // Possibly a typo, should be "green"
+        ls.add("pink");
+        ls.add("red");
+        ls.add("Blue");
+
+        // Printing the full list before removal
+        System.out.println(ls);  // Output: [Orange, geen, pink, red, Blue]
+
+        // Removing the element at index 2 ("pink")
+        ls.remove(2);
+
+        // Printing the list after removing the element at index 2
+        System.out.println(ls);  // Output: [Orange, geen, red, Blue]
+
+        // Removing the element "Blue" by value
+        ls.remove("Blue");
+
+        // Printing the list after removing "Blue"
+        System.out.println(ls);  // Output: [Orange, geen, red]
+    }
 }
+
